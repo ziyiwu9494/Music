23fab06290f2b6ff194267739dbd0d42612af55d
Ziyi Wu <ziyiw9494@gmail.com>
2019-09-30 21:11:21 -0700

    Got it playing

---
+++ app/src/main/java/com/example/music/DrawView.java
@@ -0,0 +1,36 @@
+package com.example.music;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class DrawView extends View {
+    Paint paint = new Paint();
+
+    private void init() {
+        paint.setColor(Color.BLACK);
+    }
+
+    public DrawView(Context context) {
+        super(context);
+        init();
+    }
+
+    public DrawView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public DrawView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init();
+    }
+
+    @Override
+    public void onDraw(Canvas canvas) {
+        canvas.drawLine(0, 440, 720, 440, paint);
+    }
+
+}
--- app/src/main/java/com/example/music/MainActivity.java
+++ app/src/main/java/com/example/music/MainActivity.java
@@ -1,13 +1,43 @@
 package com.example.music;
 
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.GradientDrawable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+
+import com.karlotoy.perfectune.instance.PerfectTune;
 
 public class MainActivity extends AppCompatActivity {
-
+    final PerfectTune perfectTune = new PerfectTune();
+    DrawView drawView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        drawView = new DrawView(this);
+        drawView.setBackgroundColor(Color.WHITE);
+        setContentView(drawView);
     }
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        int y = (int) event.getY();
+        switch(event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                perfectTune.setTuneFreq(y);
+                perfectTune.playTune();
+            case MotionEvent.ACTION_MOVE:
+                perfectTune.setTuneFreq(y);
+                perfectTune.playTune();
+                break;
+            case MotionEvent.ACTION_UP:
+                perfectTune.stopTune();
+                break;
+        }
+        return false;
+    }
+
 }
